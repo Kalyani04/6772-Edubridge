e21cd5893446e1b1507653ea232f3bc2d7c0d0cb
HP User <HP User@LAPTOP-5KQSLAA4>
2022-02-10 12:07:23 +0530

    My First Commit

---
+++ 6772a/src/OopsConcept/PrecedenceOpeartor.java
@@ -0,0 +1,13 @@
+package OopsConcept;
+
+public class PrecedenceOpeartor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int p = 9, q = 6, r = 4, s = 2;
+		int cal;
+		cal = p + (q*s)/r;
+		System.out.println("\n Calculation is :"+cal);
+	}
+
+}
---
+++ 6772a/src/controlStatement/Simple_if.java
@@ -0,0 +1,13 @@
+package controlStatement;
+
+public class Simple_if {
+
+	public static void main(String[] args) {
+		int i = 10; 
+		
+		if( i< 15)
+		System.out.println(" 10 is less than 15");
+
+	}
+
+}
